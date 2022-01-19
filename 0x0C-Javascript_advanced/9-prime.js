e8be65cc7fdbcebbac40aeecbf26d7d42a00740e
vanemcb <vanem2201@gmail.com>
2022-01-18 20:01:56 -0500

    Task 9 created

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+  function isPrime(num) {
+    let prime = true;
+    for (let x = 2; x < num; x++) {
+      if (num % x == 0) {
+        prime = false;
+        break;
+      }
+    }
+    return prime
+  }
+
+  let cont = 0;
+  for (i = 2; i <= 100; i++) {
+    if (isPrime(i) == true) {
+      cont++;
+    }
+  }
+  console.log(cont);
+
+}
+
+const t0 = performance.now();
+countPrimeNumbers();
+const t1 = performance.now();
+console.log('Execution time of printing countPrimeNumbers was ' + (t1 - t0) + ' milliseconds.');
