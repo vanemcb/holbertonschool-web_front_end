77ac1fff9a8871a7043d04eefe0e1cf6389485f5
vanemcb <vanem2201@gmail.com>
2022-01-18 20:33:06 -0500

    Task 12 created

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,9 @@
+roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea: function() {
+    return this.width * this.length
+  }
+}
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
